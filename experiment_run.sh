8c23d50d7138388232bb9a56df1d3f0010e1442d
DuneeshaFernando <duneesha18@gmail.com>
2020-05-25 20:03:13 +0530

    Updated with the latest observability image

--- experiment_run.sh
+++ experiment_run.sh
@@ -15,18 +15,18 @@ docker exec -i mysqlcontainerhotel mysql -u root -p$password < mySQLhotelCommand
 docker exec -i mysqlcontainercar mysql -u root -p$password < mySQLcarCommandsFile.txt
 
 #Starting the service containers
-docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:observability-improvements ballerina build --skip-tests travelAgent
+docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:latest ballerina build --skip-tests travelAgent
 docker build -t travelobs -f src/travelAgent/docker/Dockerfile ${PWD}
 docker run -dit --cpus=1 --name=ta1 -p 9298:9298 travelobs:latest;
 
-docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:observability-improvements ballerina build --skip-tests mockingServiceAirline
+docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:latest ballerina build --skip-tests mockingServiceAirline
 docker build -t airlineobs -f src/mockingServiceAirline/docker/Dockerfile ${PWD}
 docker run -dit --cpus=1 --name=mk1 -p 7278:7278 airlineobs:latest;
 
-docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:observability-improvements ballerina build --skip-tests mockingServiceHotel
+docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:latest ballerina build --skip-tests mockingServiceHotel
 docker build -t hotelobs -f src/mockingServiceHotel/docker/Dockerfile ${PWD}
 docker run -dit --cpus=1 --name=mk2 -p 6268:6268 hotelobs:latest;
 
-docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:observability-improvements ballerina build --skip-tests mockingServiceCar
+docker container run --rm -it -v $(pwd):/home/ballerina -u $(id -u):$(id -u) -e JAVA_OPTS="-Duser.home=/home/ballerina" choreoipaas/choreo-ballerina:latest ballerina build --skip-tests mockingServiceCar
 docker build -t carobs -f src/mockingServiceCar/docker/Dockerfile ${PWD}
 docker run -dit --cpus=1 --name=mk3 -p 5258:5258 carobs:latest;
